be8c43332ef4b0d35c59f14647552c08c2ce3571
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-09-07 18:04:42 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace olausuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Olá, usuário! Por favor, digite seu nome:");
+		    string nomeusuario = Console.ReadLine();
+		    Console.Write("Seja bem-vindo(a), ");
+            Console.WriteLine(nomeusuario);
+        }
+    }
+}
